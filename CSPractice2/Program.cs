a2ca0130409f5e680c692a29960db76518069c14
seonyo <jsy6535@naver.com>
2024-07-03 09:07:32 +0900

    #6 하이딩

--- CSPractice2/Program.cs
+++ CSPractice2/Program.cs
@@ -8,11 +8,43 @@ namespace CSPractice2
 {
     internal class Program
     {
+
+        class Parent
+        {
+            public int variable = 273;
+            public void Method()
+            {
+                Console.WriteLine("부모 메서드");
+            }
+        }
+
+        class Child : Parent
+        {
+            public new string variable = "하이딩";
+            public new void Method()
+            {
+                Console.WriteLine("자식 메서드");
+            }
+        }
+
+        int number = 10;
+
         static void Main(string[] args)
         {
             // #6 셰도잉
             int number = 20;
-            Console.WriteLine(number);  
+            Console.WriteLine(number);
+
+            // #6 하이딩
+            Child child = new Child();
+            Console.WriteLine(child.variable);  //출력 : 하이딩
+            child.Method(); //출력 : 자식메서드
+            Console.WriteLine(((Parent)child).variable);  // 출력 : 273
+            ((Parent)child).Method(); // 출력 : 부모메서드
+            Parent p = new Parent();
+            Console.WriteLine(p.variable); // 출력 : 273
+            p.Method(); //출력 : 부모메서드
+            ((Child)p).Method();  // 출력 : 자식 메서드
         }
     }
 }
